6c53785a92902692c61bd236783dfbb767b28ae8
José Francisco Díaz <jfcodiaz@gmail.com>
2020-08-19 01:27:22 -0500

    03 Error handlign middleware

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application, Request, Response, NextFunction } from 'express'
 import path from 'path'
 
 const app: Application = express()
@@ -8,6 +8,26 @@ app.use(express.static(staticPath))
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello Express and TypeScript')
+})
+
+app.get('/error', (req: Request, res: Response, next: NextFunction) => {
+  const logo = path.join(staticPath, 'logo.png')
+  res.sendFile(logo, function (error: Error) {
+    if (error !== null) {
+      next(new Error('Error sending file!'))
+    }
+  })
+})
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  console.log('We have a error : ', error)
+  next(error)
+})
+
+// eslint-disable-next-line handle-callback-err
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500)
+  res.send('We have a error')
 })
 
 app.listen(3001, () => console.log('Server Liste 30001'))
