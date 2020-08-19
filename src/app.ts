5684b4880491cd44e43f8f213b46a833043dd1ac
José Francisco Díaz <jfcodiaz@gmail.com>
2020-08-19 01:12:00 -0500

    02 - Middleware Static

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express, { Application, Request, Response } from 'express'
+import path from 'path'
 
 const app: Application = express()
+const staticPath = path.join(__dirname, './../public')
+
+app.use(express.static(staticPath))
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello Express and TypeScript')
